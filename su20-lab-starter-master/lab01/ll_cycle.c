3496fa4c1c5ab04c838f0520e8b632aefc3217e2
jrryzh <1393769743@qq.com>
2022-12-15 08:48:58 -0800

    commit on lab01

--- su20-lab-starter-master/lab01/ll_cycle.c
+++ su20-lab-starter-master/lab01/ll_cycle.c
@@ -1,7 +1,26 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node *tortoise = head, *hare = head;
+
+    if (!head || !(*head).next || !(*(*head).next).next) {
+        return 0;
+    } else {
+        tortoise = (*tortoise).next;
+        hare = (*(*hare).next).next;
+    }
+
+    while (tortoise != hare) {
+        if (!(*hare).next || !(*(*hare).next).next) {
+            return 0;
+        } else {
+            tortoise = (*tortoise).next;
+            hare = (*(*hare).next).next;
+        }
+    }
+    return 1;
+
 }
