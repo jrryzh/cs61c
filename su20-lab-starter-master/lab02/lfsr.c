82ac12429228069cb120e696619234a83ad15af0
jrryzh <1393769743@qq.com>
2022-12-30 16:02:36 +0800

    finish lab02

--- su20-lab-starter-master/lab02/lfsr.c
+++ su20-lab-starter-master/lab02/lfsr.c
@@ -4,7 +4,16 @@
 #include <string.h>
 #include "lfsr.h"
 
+uint16_t get_bit(uint16_t * x, uint16_t n) {
+    return * x >> n << 15 >> 15;
+}
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t a;
+    a = get_bit(reg, 0) ^  get_bit(reg, 2);
+    a = get_bit(reg, 3) ^ a;
+    a = get_bit(reg, 5) ^ a;
+    * reg = (* reg >> 1) + (a << 15);
 }
 
