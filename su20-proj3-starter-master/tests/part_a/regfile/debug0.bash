a81d92044d694e682c8588e17de661bed292057e
jrryzh <1393769743@qq.com>
2023-01-20 23:07:11 +0800

    1st half on task3

--- su20-proj3-starter-master/tests/part_a/regfile/debug0.bash
+++
@@ -1,3 +0,0 @@
-python3 binary_to_hex_regfile.py reference_output/regfile-allregs-ref.out > reference.out
-python3 binary_to_hex_regfile.py student_output/regfile-allregs-student.out > student.out
-diff reference.out student.out
